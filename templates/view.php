5ec90f56d7766ceb79c19ce89e115569211943e9
Pre <pre@dalliance.net>
2026-01-02 16:50:51 +0100

    Fix to actually hide when shearelinks is hidden

--- templates/view.php
+++ templates/view.php
@@ -1,18 +1,18 @@
 <div class="row">
-
-	<div class="shareIcons">
-		<p>🌎 Share</p>
-		<ul>
-			<li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" class="btn">Facebook</a></li>
-			<li><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo $url; ?>" class="btn">Twitter</a></li>
-			<li><a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo $url; ?>" class="btn">Pinterest</a></li>
-			<li><a target="_blank" href="https://t.me/share/url?url=<?php echo $url; ?>" class="btn">Telegram</a></li>
-			<li><a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>" class="btn">LinkedIn</a></li>
-			<li><a target="_blank" href="mailto:?&subject=&cc=&bcc=&body=<?php echo $url; ?>" class="btn">Email</a></li>
-			<li><a id="clipcopy" class="btn">Clipboard</a></li>
-		</ul>
-	</div>
-
+  <?php if($sharelinks!="1"): ?>
+    <div class="shareIcons">
+      <p>🌎 Share</p>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" class="btn">Facebook</a></li>
+        <li><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo $url; ?>" class="btn">Twitter</a></li>
+        <li><a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo $url; ?>" class="btn">Pinterest</a></li>
+        <li><a target="_blank" href="https://t.me/share/url?url=<?php echo $url; ?>" class="btn">Telegram</a></li>
+        <li><a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>" class="btn">LinkedIn</a></li>
+        <li><a target="_blank" href="mailto:?&subject=&cc=&bcc=&body=<?php echo $url; ?>" class="btn">Email</a></li>
+        <li><a id="clipcopy" class="btn">Clipboard</a></li>
+      </ul>
+    </div>
+  <?php endif; ?>
 
 	<div class="messageText">
 		<div class="messageBody">
